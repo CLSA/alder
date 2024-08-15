deb44d1e402ce290caee6b5cd73e4f2c226fb1a9
Patrick Emond <emondpd@mcmaster.ca>
2024-08-15 16:47:46 -0400

    Bug: couldn't edit image note from review page

---
+++ src/service/review/patch.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * patch.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ */
+
+namespace alder\service\review;
+use cenozo\lib, cenozo\log, alder\util;
+
+class patch extends \cenozo\service\patch
+{
+  /**
+   * Extends parent method
+   */
+  protected function prepare()
+  {
+    $this->extract_parameter_list[] = 'note';
+    parent::prepare();
+  }
+
+  /**
+   * Extends parent method
+   */
+  protected function execute()
+  {
+    parent::execute();
+
+    // set the parent image's note if it was provided as part of the patch
+    $note = $this->get_argument( 'note', false );
+    if( false !== $note )
+    {
+      $db_image = $this->get_leaf_record()->get_image();
+      $db_image->note = $note;
+      $db_image->save();
+    }
+  }
+}
