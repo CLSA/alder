7e746ad2458934de18d2323c6556b172a9d9fe4a
Patrick Emond <emondpd@mcmaster.ca>
2025-09-19 10:30:27 -0400

    Feature: Restricting typist home list and nav buttons to state=Uploaded only

--- src/database/base_review.class.php
+++ src/database/base_review.class.php
@@ -22,13 +22,14 @@ abstract class base_review extends \cenozo\database\record
   }
 
   /**
-   * Returns the previous and next interview and exam related to this review (by user)
+   * Returns the previous and next interview and exam related to this review
    */
   public function get_neighbouring_reviews()
   {
     $interview_class_name = lib::get_class_name( 'database\interview' );
     $exam_class_name = lib::get_class_name( 'database\exam' );
 
+    $is_typist = 'typist' == lib::create( 'business\session' )->get_role()->name;
     $review_table = static::get_table_name();
     $db_current_exam = $this->get_exam();
     $db_current_scan_type = $db_current_exam->get_scan_type();
@@ -43,17 +44,39 @@ abstract class base_review extends \cenozo\database\record
       'next_interview_review_id' => NULL
     ];
 
-    // Get the previous and next interviews for this user.
-    // This is done by getting a list of all interviews that the user has a review for, sorting them by
-    // uid (for apex reviews) or site and uid (for non-apex reviews), then finding the interview before
-    // and after the current interview.
+    // Get the previous and next interview
+    // Typists will only see their own available reviews, other roles will see all reviews
+    // This is done by getting a list of all interviews, sorting them by uid (for apex reviews) or site and uid
+    // (for non-apex reviews), then finding the interview before and after the current interview.
     $interview_mod = lib::create( 'database\modifier' );
     $interview_mod->join( 'participant', 'interview.participant_id', 'participant.id' );
     $interview_mod->join( 'exam', 'interview.id', 'exam.interview_id' );
     $interview_mod->join( $review_table, 'exam.id', sprintf( '%s.exam_id', $review_table ) );
     if( 'review' == $review_table ) $interview_mod->left_join( 'site', 'interview.site_id', 'site.id' );
     $interview_mod->where( 'interview.study_phase_id', '=', $db_current_interview->study_phase_id );
-    $interview_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+    if( $is_typist )
+    {
+      $interview_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+
+      // also restrict typists to reviews that have been uploaded
+      if( 'apex_review' == $review_table )
+      {
+        $interview_mod->join(
+          'apex_review_effective_apex_analysis',
+          'apex_review.id',
+          'apex_review_effective_apex_analysis.apex_review_id'
+        );
+        $interview_mod->join(
+          'apex_analysis',
+          'apex_review_effective_apex_analysis.apex_analysis_id',
+          'apex_analysis.id'
+        );
+        $interview_mod->where( 'apex_review.end_datetime', '=', NULL );
+        $interview_mod->where( 'apex_analysis.download_datetime', '=', NULL );
+        $interview_mod->where( 'apex_analysis.upload_datetime', '!=', NULL );
+        $interview_mod->where( 'apex_analysis.upload_status', '=', NULL );
+      }
+    }
     if( 'review' == $review_table ) $interview_mod->order( 'site.name' );
     $interview_mod->order( 'participant.uid' );
 
@@ -92,7 +115,7 @@ abstract class base_review extends \cenozo\database\record
       $prev_index = $current_index - 1;
       if( array_key_exists( $prev_index, $interview_list ) )
       {
-        // get the user's first review for the prev interview (sorted by scan type)
+        // get the first review for the prev interview (sorted by scan type)
         $exam_sel = lib::create( 'database\select' );
         $exam_sel->add_table_column( $review_table, 'id' );
         $exam_mod = lib::create( 'database\modifier' );
@@ -109,7 +132,7 @@ abstract class base_review extends \cenozo\database\record
       $next_index = $current_index + 1;
       if( array_key_exists( $next_index, $interview_list ) )
       {
-        // get the user's first review for the next interview (sorted by scan type)
+        // get the first review for the next interview (sorted by scan type)
         $exam_sel = lib::create( 'database\select' );
         $exam_sel->add_table_column( $review_table, 'id' );
         $exam_mod = lib::create( 'database\modifier' );
@@ -124,22 +147,62 @@ abstract class base_review extends \cenozo\database\record
       }
     }
 
-    // Get the previous and next exam for this user.
-    // This is done by getting the previous or next exam belonging to the same interview in scan-type
-    // alphabetical order that the user is also assigned to which also belongs to the same interview
-    // as the current review.
-
+    // Get the previous and next exam
     $base_exam_mod = lib::create( 'database\modifier' );
     $base_exam_mod->join( 'scan_type', 'exam.scan_type_id', 'scan_type.id' );
     $base_exam_mod->join( $review_table, 'exam.id', sprintf( '%s.exam_id', $review_table ) );
     $base_exam_mod->where( 'exam.interview_id', '=', $db_current_interview->id );
-    $base_exam_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+    if( $is_typist )
+    {
+      $base_exam_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+
+      // also restrict typists to reviews that have been uploaded
+      if( 'apex_review' == $review_table )
+      {
+        $base_exam_mod->join(
+          'apex_review_effective_apex_analysis',
+          'apex_review.id',
+          'apex_review_effective_apex_analysis.apex_review_id'
+        );
+        $base_exam_mod->join(
+          'apex_analysis',
+          'apex_review_effective_apex_analysis.apex_analysis_id',
+          'apex_analysis.id'
+        );
+        $base_exam_mod->where( 'apex_review.end_datetime', '=', NULL );
+        $base_exam_mod->where( 'apex_analysis.download_datetime', '=', NULL );
+        $base_exam_mod->where( 'apex_analysis.upload_datetime', '!=', NULL );
+        $base_exam_mod->where( 'apex_analysis.upload_status', '=', NULL );
+      }
+    }
     $base_exam_mod->limit( 1 );
 
     $base_review_mod = lib::create( 'database\modifier' );
     $base_review_mod->join( 'scan_type', 'exam.scan_type_id', 'scan_type.id' );
     $base_review_mod->join( $review_table, 'exam.id', sprintf( '%s.exam_id', $review_table ) );
-    $base_review_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+    if( $is_typist )
+    {
+      $base_review_mod->where( sprintf( '%s.user_id', $review_table ), '=', $this->user_id );
+
+      // also restrict typists to reviews that have been uploaded
+      if( 'apex_review' == $review_table )
+      {
+        $base_exam_mod->join(
+          'apex_review_effective_apex_analysis',
+          'apex_review.id',
+          'apex_review_effective_apex_analysis.apex_review_id'
+        );
+        $base_exam_mod->join(
+          'apex_analysis',
+          'apex_review_effective_apex_analysis.apex_analysis_id',
+          'apex_analysis.id'
+        );
+        $base_exam_mod->where( 'apex_review.end_datetime', '=', NULL );
+        $base_exam_mod->where( 'apex_analysis.download_datetime', '=', NULL );
+        $base_exam_mod->where( 'apex_analysis.upload_datetime', '!=', NULL );
+        $base_exam_mod->where( 'apex_analysis.upload_status', '=', NULL );
+      }
+    }
     $base_review_mod->order( 'scan_type.name' );
     $base_review_mod->order( 'scan_type.side' );
     $base_review_mod->limit( 1 );
